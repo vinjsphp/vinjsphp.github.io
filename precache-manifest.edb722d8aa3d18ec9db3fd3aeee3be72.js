27dbdc649d3a123e5b6e222f0c6711c4efd7b3e2
Vin <vinjsphp@gmail.com>
2020-01-17 08:46:46 +0800

    Router test 1

---
+++ precache-manifest.edb722d8aa3d18ec9db3fd3aeee3be72.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d01774a1d43f481f5cf6f600e06644ee",
+    "url": "/index.html"
+  },
+  {
+    "revision": "34534fa8a68816b4e11a",
+    "url": "/static/css/main.af0fdb64.chunk.css"
+  },
+  {
+    "revision": "7164f178a7659fec2171",
+    "url": "/static/js/2.60b56a8f.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/static/js/2.60b56a8f.chunk.js.LICENSE"
+  },
+  {
+    "revision": "34534fa8a68816b4e11a",
+    "url": "/static/js/main.3abd38f3.chunk.js"
+  },
+  {
+    "revision": "0da30eef4ae713eb5e2f",
+    "url": "/static/js/runtime-main.9868ccd5.js"
+  }
+]);
